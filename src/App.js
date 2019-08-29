47cf85f328b0c1a286ee7a988895e376b15df0b0
Andress <andress@tbltechnerds.com>
2019-08-29 19:58:08 +0600

    Initial updates
    Initial updates

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import './App.css';
+import './board'
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+    }
+
+    //The initial setup
+    gameBoard = [
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0]
+    ];
+    //arrays to store the instances
+    pieces = [];
+    tiles = [];
+
+    //distance formula
+    dist = function (x1, y1, x2, y2) {
+        return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+    }
+
+    render() {
+        return (
+            <div className='app'>
+                <div className="column justifyCenter">
+                    <div className="stats">
+                        <div className="wrapper">
+                            <div id="player1">
+                                <h3>Player 1</h3>
+                            </div>
+                            <div id="player2">
+                                <h3>Player 2</h3>
+                            </div>
+                        </div>
+                        <div className="clearfix"></div>
+                        <div className="turn"></div>
+                        <span id="winner"></span>
+                        <button id="cleargame">Reset Game</button>
+                    </div>
+                </div>
+                <div className="column justifyCenter">
+                    <div id="board">
+                        <div className="tiles"></div>
+                        <div className="pieces">
+                            <div className="player2pieces">
+                            </div>
+                            <div className="player1pieces">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
